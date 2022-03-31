c535e65c8551775be276d4d5f5a027caa222795b
Manisha Kumari <krimanisha666@gmail.com>
2022-03-31 16:08:55 +0530

    added 1st code

---
+++ positive_negative.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n>=0){
+        cout<<"Positive Number"<<endl;
+    }
+    else{
+        cout<<"Negative Number"<<endl;
+    }
+}
