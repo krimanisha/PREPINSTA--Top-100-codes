fdd740d1a6c4e3edffd2d9477992e68fdc5de5be
Manisha Kumari <krimanisha666@gmail.com>
2022-03-31 16:19:33 +0530

    added

---
+++ 2_even_odd.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Even"<<endl;
+    }
+    else{
+        cout<<"Odd"<<endl;
+    }
+}
