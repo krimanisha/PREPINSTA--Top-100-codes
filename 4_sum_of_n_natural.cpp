d59b2779bf784bd535993f74e8a453808fb2b22f
Manisha Kumari <krimanisha666@gmail.com>
2022-04-01 22:57:10 +0530

    added

---
+++ 4_sum_of_n_natural.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
---
+++ 6_greatest_two_no.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<a<<endl;
+    }
+    else{
+        cout<<b<<endl;
+    }
+}
