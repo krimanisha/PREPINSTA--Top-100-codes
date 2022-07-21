006abc0467bf110419d023a0492dfd2018323302
Manisha Kumari <krimanisha666@gmail.com>
2022-07-21 16:41:28 +0530

    added

--- Important Codes related to Arrays/05.cpp
+++ Important Codes related to Arrays/05.cpp
@@ -1,1 +1,20 @@
+//Calculate the sum of elements in an array
 
+#include<bits/stdc++.h>
+using namespace std;
+void sumElements(int arr[], int n){
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum+=arr[i];
+    }
+    cout<<sum<<endl;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sumElements(arr,n);
+}
